baac0b9471705a9c902be71f4576e50254997cf9
wellbell <uok02018496@naver.com>
2021-06-20 21:01:08 +0900

    BATCH_JOB_EXECUTION 실습

--- src/main/java/com/spring/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/spring/batch/job/SimpleJobConfiguration.java
@@ -23,6 +23,7 @@ public class SimpleJobConfiguration {
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
@@ -31,7 +32,20 @@ public class SimpleJobConfiguration {
     public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
+                    //throw new IllegalArgumentException("step1에서 실패합니다.");
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
                     log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
