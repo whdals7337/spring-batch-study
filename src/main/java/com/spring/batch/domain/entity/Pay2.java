b511a6a259609ab8700db8b4d42390186dcae321
wellbell <uok02018496@naver.com>
2021-06-21 19:22:07 +0900

    ItemWriter (jdbc, jpa, custom) 실습

---
+++ src/main/java/com/spring/batch/domain/entity/Pay2.java
@@ -0,0 +1,33 @@
+package com.spring.batch.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Pay2 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long amount;
+    private String txName;
+    private LocalDateTime txDateTime;
+
+    public Pay2(Long amount, String txName, LocalDateTime txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+}
